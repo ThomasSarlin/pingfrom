25b96a058cd4f6d3a85a310a7b88db0f1deddfb6
ThomasSarlin <thomas.sarlin@gmail.com>
2018-01-17 13:51:39 +0100

    Added data to each page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -15,10 +15,21 @@ class PagesController extends Controller
         return view('pages.index')->with($data);
     }
     public function chat(){
-        return view('pages.chat');
+        $data = array(
+            'title'=>'Welcome to the Chat',
+            'text' => 'Here you can represent your country in a fierce discussion',
+            'buttonText' =>'Send reply',
+            'fieldText' =>'Apply creative message:'
+        );
+        return view('pages.chat')-with($data);
     }
     public function stats(){
-        return view('pages.stats');
+        $data =array(
+            'title' => 'Statistics overview!',
+            'text' => 'Embark upon a magical statistic jouney!',
+            'buttonText' => 'Update'
+        );
+        return view('pages.stats')->with($data);
     }
     public function ping(){
         $data = array(
