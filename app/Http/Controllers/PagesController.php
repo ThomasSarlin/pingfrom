e2dd50613bde4bc370924af90dcbff8686a14ae0
ThomasSarlin <thomas.sarlin@gmail.com>
2018-01-17 09:46:12 +0100

    initial setup

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        return view('pages.index');
+    }
+    public function about(){
+        return view('pages.about');
+    }
+    public function stats(){
+        return view('pages.stats');
+    }
+    public function ping(){
+        $data = array(
+            'title'=>'Add your ping'
+        );
+        return view('pages.ping')->with($data);
+    }
+}
